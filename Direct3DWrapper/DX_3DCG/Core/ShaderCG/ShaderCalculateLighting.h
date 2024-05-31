c6b597e12219d2010ae5699072fa439c060154e0
satoshi.s <satoshi.sugawara19790115@gmail.com>
2024-06-01 00:07:17 +0900

    リファクタリング
    ShaderCalculateLighting.hlsl
    リファクタリング

--- Direct3DWrapper/DX_3DCG/Core/ShaderCG/ShaderCalculateLighting.h
+++ Direct3DWrapper/DX_3DCG/Core/ShaderCG/ShaderCalculateLighting.h
@@ -3,13 +3,41 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 char* ShaderCalculateLighting =
-//////////////////////////////////////ライト計算/////////////////////////////////////////////////////////
+//////////////////////////////////////ライト計算OutPut/////////////////////////////////////////////////////
 "struct LightOut\n"
 "{\n"
 "    float3 Diffuse: COLOR;\n"
 "    float3 Speculer: COLOR;\n"
 "};\n"
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////ランバート拡散反射/////////////////////////////////////////////////
+"float3 Lambert(float distAtten, float3 lightCol, float3 Diffuse, float3 Ambient, float3 Nor, float3 LVec)\n"
+"{\n"
+//角度減衰率ディフェーズ
+//内積で計算するので光源のベクトルを逆にする
+"    float angleAttenDif = saturate(dot(-LVec, Nor));\n"
+
+"    return distAtten * lightCol * angleAttenDif * Diffuse + Ambient;\n"
+"}\n"
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////フォン鏡面反射/////////////////////////////////////////////////////
+"float3 Phong(float distAtten, float3 lightCol, float3 SpeculerCol, float3 Nor, float3 LVec, \n"
+"             float3 wPos, float3 eyePos, float shininess)\n"
+"{\n"
+//視線ベクトル
+"    float3 eyeVec = normalize(wPos - eyePos);\n"
+//反射ベクトル
+"    float3 reflectVec = normalize(reflect(LVec, Nor));\n"
+//角度減衰率スペキュラ
+"    float angleAttenSpe = pow(saturate(dot(reflectVec, -eyeVec)), shininess);\n"
+
+"    return distAtten * lightCol * angleAttenSpe * SpeculerCol;\n"
+"}\n"
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////ライト計算/////////////////////////////////////////////////////////
 "LightOut LightCom(float3 SpeculerCol, float3 Diffuse, float3 Ambient, float3 Nor, \n"
 "                  float3 wPos, float3 lightCol, float3 eyePos, float3 lightVec, float distAtten, float shininess)\n"
 "{\n"
@@ -18,20 +46,11 @@ char* ShaderCalculateLighting =
 
 //ライトベクトル正規化
 "    float3 LVec = normalize(lightVec);\n"
-//角度減衰率ディフェーズ
-"    float angleAttenDif = saturate(dot(LVec, Nor));"
-
-//視線ベクトル
-"    float3 eyeVec = normalize(eyePos - wPos);\n"
-//反射ベクトル
-"    float3 reflectVec = reflect(-LVec, Nor);\n"
-//角度減衰率スペキュラ
-"    float angleAttenSpe = pow(saturate(dot(eyeVec, reflectVec)), shininess);\n"
 
 //ディフェーズ出力
-"    Out.Diffuse = distAtten * lightCol * angleAttenDif * Diffuse + Ambient;\n"
+"    Out.Diffuse = Lambert(distAtten, lightCol, Diffuse, Ambient, Nor, LVec);\n"
 //スペキュラ出力
-"    Out.Speculer = distAtten * lightCol * angleAttenSpe * SpeculerCol;\n"
+"    Out.Speculer = Phong(distAtten, lightCol, SpeculerCol, Nor, LVec, wPos, eyePos, shininess);\n"
 "    return Out;\n"
 "}\n"
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -43,8 +62,8 @@ char* ShaderCalculateLighting =
 //出力用
 "    LightOut Out = (LightOut)0;\n"
 
-//ライトベクトル
-"    float3 lightVec = lightPos.xyz - wPos;\n"
+//ライトベクトル (頂点の位置 - 点光源の位置)
+"    float3 lightVec = wPos - lightPos.xyz;\n"
 
 //頂点から光源までの距離を計算
 "    float distance = length(lightVec);\n"
@@ -79,7 +98,7 @@ char* ShaderCalculateLighting =
 
 //光源計算
 "       Out = LightCom(SpeculerCol, Diffuse, Ambient, Nor, \n"
-"                      wPos, DCol, eyePos, -Dir, 1.0f, shininess);\n"
+"                      wPos, DCol, eyePos, Dir, 1.0f, shininess);\n"
 
 "    }\n"
 "    return Out;\n"
